1426c4128bfb94844dbff4b9374a03023ed71fac
dazanestor <dazanestor@gmail.com>
2024-07-22 21:55:09 +0200

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Crea el directorio de configuración si no existe
+mkdir -p /app/config
+
+# Copia el archivo de configuración por defecto si no existe
+if [ ! -f /app/config/config.json ]; then
+  cp /app/config/config.json.default /app/config/config.json
+fi
+
+exec "$@"
