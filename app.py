769d9ba2f31910584e9a4c040b61c0c3a00acedb
dazanestor <dazanestor@gmail.com>
2024-07-24 21:52:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -27,14 +27,8 @@ class ConfigSchema(Schema):
     sonarr_root_folder_path = fields.Str(required=True)
 
 def read_config():
-    return {
-        'radarr_url': os.getenv('RADARR_URL', 'http://localhost:7878'),
-        'radarr_api_key': os.getenv('RADARR_API_KEY', 'default_radarr_api_key'),
-        'sonarr_url': os.getenv('SONARR_URL', 'http://localhost:8989'),
-        'sonarr_api_key': os.getenv('SONARR_API_KEY', 'default_sonarr_api_key'),
-        'tmdb_api_key': os.getenv('TMDB_API_KEY', 'default_tmdb_api_key'),
-        'redis_ip': os.getenv('REDIS_IP', 'redis')
-    }
+    with open('config.json', 'r') as f:
+        return json.load(f)
 
 def write_config(data):
     with open('config.json', 'w') as f:
@@ -52,27 +46,34 @@ def get_sonarr_profiles_and_paths(sonarr_url, sonarr_api_key):
     paths = requests.get(f"{sonarr_url}/api/v3/rootFolder", headers=headers).json()
     return profiles, paths
 
-config_env = read_config()
+config_env = {
+    'radarr_url': os.getenv('RADARR_URL', 'http://localhost:7878'),
+    'radarr_api_key': os.getenv('RADARR_API_KEY', 'default_radarr_api_key'),
+    'sonarr_url': os.getenv('SONARR_URL', 'http://localhost:8989'),
+    'sonarr_api_key': os.getenv('SONARR_API_KEY', 'default_sonarr_api_key'),
+    'tmdb_api_key': os.getenv('TMDB_API_KEY', 'default_tmdb_api_key'),
+    'redis_ip': os.getenv('REDIS_IP', 'redis')
+}
 r = redis.Redis(host=config_env['redis_ip'], port=6379, db=0)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    config = read_config()
     radarr_profiles, radarr_paths = [], []
     sonarr_profiles, sonarr_paths = [], []
 
-    if config['radarr_api_key'] and config['radarr_url']:
+    if config_env['radarr_api_key'] and config_env['radarr_url']:
         try:
-            radarr_profiles, radarr_paths = get_radarr_profiles_and_paths(config['radarr_url'], config['radarr_api_key'])
+            radarr_profiles, radarr_paths = get_radarr_profiles_and_paths(config_env['radarr_url'], config_env['radarr_api_key'])
         except requests.exceptions.RequestException as e:
             flash(f"Error al obtener perfiles y rutas de Radarr: {str(e)}")
 
-    if config['sonarr_api_key'] and config['sonarr_url']:
+    if config_env['sonarr_api_key'] and config_env['sonarr_url']:
         try:
-            sonarr_profiles, sonarr_paths = get_sonarr_profiles_and_paths(config['sonarr_url'], config['sonarr_api_key'])
+            sonarr_profiles, sonarr_paths = get_sonarr_profiles_and_paths(config_env['sonarr_url'], config_env['sonarr_api_key'])
         except requests.exceptions.RequestException as e:
             flash(f"Error al obtener perfiles y rutas de Sonarr: {str(e)}")
 
+    config = read_config()
     imported_movies = json.loads(r.get('imported_movies') or '[]')
     imported_series = json.loads(r.get('imported_series') or '[]')
 
